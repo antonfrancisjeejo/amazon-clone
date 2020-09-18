3e675509504d08b53523dc25d80be03e7e7dadbd
unknown <anton13jeejo@gmail.com>
2020-09-18 23:18:21 +0530

    Initial commit

---
+++ src/pages/Checkout/Checkout.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { useStateValue } from "../../components/StateProvider/StateProvider";
+import "./Checkout.css";
+import CheckoutProduct from "../../components/CheckoutProduct/CheckoutProduct";
+import Subtotal from "../../components/Subtotal/Subtotal";
+
+const Checkout = () => {
+  const [{ basket }] = useStateValue();
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          className="checkout__ad"
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          alt=""
+        />
+        {basket?.length === 0 ? (
+          <div>
+            <h2>Your Shopping Basket is empty</h2>
+            <p>
+              You have no items in your basket. To buy or more items, click "Add
+              to basket" next to the item.
+            </p>
+          </div>
+        ) : (
+          <div>
+            <h2 className="checkout__title">Your shopping basket</h2>
+            {basket.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+      {basket.length > 0 && (
+        <div className="checkout__right">
+          <Subtotal />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Checkout;
