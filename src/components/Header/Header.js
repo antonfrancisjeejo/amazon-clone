3fcaff39dafdf3e3377859b291db6c674b531bd5
unknown <anton13jeejo@gmail.com>
2020-09-20 01:33:44 +0530

    Initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,63 @@
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { useStateValue } from "../StateProvider/StateProvider";
+import { auth } from "../../firebase";
+
+const Header = () => {
+  const [{ basket, user }] = useStateValue();
+  const login = () => {
+    if (user) {
+      auth.signOut();
+    }
+  };
+  return (
+    <nav className="header">
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="https://pngimg.com/uploads/amazon/amazon_PNG25.png"
+          alt="logo"
+        />
+      </Link>
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+      <div className="header__nav">
+        <Link to={!user && "/login"} className="header__link">
+          <div className="header__option" onClick={login}>
+            <span className="header__optionLineOne">Hello {user?.email}</span>
+            <span className="header__optionLineTwo">
+              {user ? "Sign Out" : "Sign In"}
+            </span>
+          </div>
+        </Link>
+        <Link to="/" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Return</span>
+            <span className="header__optionLineTwo"> & Orders</span>
+          </div>
+        </Link>
+        <Link to="/" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Your</span>
+            <span className="header__optionLineTwo">Prime</span>
+          </div>
+        </Link>
+        <Link to="/checkout" className="header__link">
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header__optionLineTwo header__basketCount">
+              {basket.length}
+            </span>
+          </div>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
