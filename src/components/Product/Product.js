0cfb039b0610860430920e5d97260f29954779f6
unknown <anton13jeejo@gmail.com>
2020-09-21 10:28:53 +0530

    Initial commit

---
+++ src/components/Product/Product.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Product.css";
+import { useStateValue } from "../StateProvider/StateProvider";
+
+const Product = ({ id, title, image, price, rating }) => {
+  const [{ basket }, dispatch] = useStateValue();
+  const addToBasket = () => {
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id,
+        title,
+        image,
+        price,
+        rating,
+      },
+    });
+  };
+
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
+          <small>₹</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_) => (
+              <span role="img" aria-label="rating">
+                ⭐
+              </span>
+            ))}
+        </div>
+      </div>
+      <img src={image} alt="" />
+      <button onClick={addToBasket}>Add to basket</button>
+    </div>
+  );
+};
+
+export default Product;
